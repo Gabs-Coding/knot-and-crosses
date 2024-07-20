14cec16ba52ba6cb3859e9f1189d84bc86b700ba
Gabriel Soares Ribeiro <gsbiel62@gmail.com>
2024-07-20 09:10:45 -0400

    Add the initializer of the game session of the Knoth and Crosses game.

--- src/com/gabscoding/knotsandcrosses/KnotsAndCrosses.java
+++ src/com/gabscoding/knotsandcrosses/KnotsAndCrosses.java
@@ -1,13 +1,12 @@
 package com.gabscoding.knotsandcrosses;
 
-import com.gabscoding.knotsandcrosses.model.Grid;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.util.Date;
+import com.gabscoding.knotsandcrosses.model.GameSession;
 
 public class KnotsAndCrosses {
-    public static void main(String args[]) {
-        Grid theGridOfTheGame = Grid.getInstance();
+
+    public static void main(String[] args) {
+        GameSession gameSession = new GameSession();
+        gameSession.listPlayersOfThisSession();
+        gameSession.letsPlay();
     }
 }
