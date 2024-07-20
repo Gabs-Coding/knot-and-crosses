15526fa00a5073c07b354680d6a83248a24424ad
Gabriel Soares Ribeiro <gsbiel62@gmail.com>
2024-07-20 08:19:52 -0400

    Adjust the attribution of the String with text block.

--- src/com/gabscoding/knotsandcrosses/model/GameMenuDirector.java
+++ src/com/gabscoding/knotsandcrosses/model/GameMenuDirector.java
@@ -4,8 +4,11 @@ public class GameMenuDirector {
     public void constructSimpleGameMenu(SimpleGameMenuBuilder builder) {
         String headerOfGameMenu = "KNOTH AND CROSSES: THE CONSOLE GAME" +
                 "\nThe beloved classic game.";
-        String optionsOfGameMenu = "1 \t Iniciar um novo jogo" +
-                "\n2 \t Ver histórico de jogos" + "\n3 \t Fechar o jogo";
+        String optionsOfGameMenu = """
+                1 \t Iniciar um novo jogo\
+
+                2 \t Ver histórico de jogos
+                3 \t Fechar o jogo""";
         builder.buildGameMenuHeader(headerOfGameMenu);
         builder.buildGameMenuOptions(optionsOfGameMenu);
     }
