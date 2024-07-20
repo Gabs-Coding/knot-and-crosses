503cd011134d1fc70eeff1ad354def936633be4b
Gabriel Soares Ribeiro <gsbiel62@gmail.com>
2024-07-20 09:09:11 -0400

    Add a class that structures a simple game menu.

---
+++ src/com/gabscoding/knotsandcrosses/model/SimpleGameMenu.java
@@ -0,0 +1,22 @@
+package com.gabscoding.knotsandcrosses.model;
+
+public class SimpleGameMenu {
+    private String header;
+    private String options;
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getOptions() {
+        return options;
+    }
+
+    public void setOptions(String options) {
+        this.options = options;
+    }
+}
