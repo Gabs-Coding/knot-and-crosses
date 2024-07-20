e2af220455d7723bbf3ac90761dab3b1b22bd33b
Gabriel Soares Ribeiro <gsbiel62@gmail.com>
2024-07-20 09:00:29 -0400

    Add annotation NotNull

--- src/com/gabscoding/knotsandcrosses/registration/GameSessionRegistration.java
+++ src/com/gabscoding/knotsandcrosses/registration/GameSessionRegistration.java
@@ -1,11 +1,12 @@
 package com.gabscoding.knotsandcrosses.registration;
 
 import com.gabscoding.knotsandcrosses.log.GameSessionLogFile;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDate;
 
 public class GameSessionRegistration {
-    public static String createTheNameOfTheGameSession() {
+    public static @NotNull String createTheNameOfTheGameSession() {
         StringBuilder nameOfTheGameSession = new StringBuilder();
         LocalDate localDate = LocalDate.now();
         nameOfTheGameSession.append(localDate);
